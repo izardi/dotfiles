e4a3b3aa429396b1f1c378efb3dcf0a570b536b3
zard <yuhh.cs@gmail.com>
2023-05-31 21:07:17 +0800

    update keymap for bufferline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -244,6 +244,75 @@ require("lazy").setup({
             })
         end
     },
+    
+    { -- todo-comments
+
+        "folke/todo-comments.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        opts = {
+            signs = true, -- show icons in the signs column
+            sign_priority = 8, -- sign priority
+            -- keywords recognized as todo comments
+            keywords = {
+                FIX = {
+                    icon = " ", -- icon used for the sign, and in search results
+                    color = "error", -- can be a hex color, or a named color (see below)
+                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                    -- signs = false, -- configure signs for some keywords individually
+                },
+                TODO = { icon = " ", color = "info" },
+                HACK = { icon = " ", color = "warning" },
+                WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+            },
+            gui_style = {
+                fg = "NONE", -- The gui style to use for the fg highlight group.
+                bg = "BOLD", -- The gui style to use for the bg highlight group.
+            },
+            merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+            -- highlighting of the line containing the todo comment
+            -- * before: highlights before the keyword (typically comment characters)
+            -- * keyword: highlights of the keyword
+            -- * after: highlights after the keyword (todo text)
+            highlight = {
+                multiline = true, -- enable multine todo comments
+                multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+                multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+                before = "", -- "fg" or "bg" or empty
+                keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                after = "fg", -- "fg" or "bg" or empty
+                pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+                comments_only = true, -- uses treesitter to match keywords in comments only
+                max_line_len = 400, -- ignore lines longer than this
+                exclude = {}, -- list of file types to exclude highlighting
+            },
+
+            colors = {
+                error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+                warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+                info = { "DiagnosticInfo", "#2563EB" },
+                hint = { "DiagnosticHint", "#10B981" },
+                default = { "Identifier", "#7C3AED" },
+                test = { "Identifier", "#FF00FF" }
+            },
+            search = {
+                command = "rg",
+                args = {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                },
+                -- regex that will be used to match keywords.
+                -- don't replace the (KEYWORDS) placeholder
+                pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+                -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+            },
+        }
+    },
 
     { -- lualine
 
@@ -513,10 +582,86 @@ require("lazy").setup({
 
         'akinsho/bufferline.nvim',
         version = "*",
+        lazy = false,
         dependencies = 'nvim-tree/nvim-web-devicons',
         config = function()
-            require("bufferline").setup{}
-        end
+            require("bufferline").setup({
+                options = {
+                    mode = "buffers", -- set to "tabs" to only show tabpages instead
+                    numbers = "ordinal", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+                    --- @deprecated, please specify numbers as a function to customize the styling
+                    -- number_style = "superscript", --| "subscript" | "" | { "none", "subscript" }, -- buffer_id at index 1, ordinal at index 2
+                    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
+                    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+                    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+                    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+                    -- NOTE: this plugin is designed with this icon in mind,
+                    -- and so changing this is NOT recommended, this is intended
+                    -- as an escape hatch for people who cannot bear it for whatever reason
+                    indicator_icon = '▎',
+                    buffer_close_icon = '',
+                    modified_icon = '●',
+                    close_icon = '',
+                    left_trunc_marker = '',
+                    right_trunc_marker = '',
+                    --- name_formatter can be used to change the buffer's label in the bufferline.
+                    --- Please note some names can/will break the
+                    --- bufferline so use this at your discretion knowing that it has
+                    --- some limitations that will *NOT* be fixed.
+                    name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+                        -- remove extension from markdown files for example
+                        if buf.name:match('%.md') then
+                            return vim.fn.fnamemodify(buf.name, ':t:r')
+                        end
+                    end,
+                    max_name_length = 18,
+                    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+                    tab_size = 18,
+                    diagnostics = false, --| "nvim_lsp" | "coc",
+                    diagnostics_update_in_insert = false,
+                    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                        return "("..count..")"
+                    end,
+                    -- NOTE: this will be called a lot so don't do any heavy processing here
+                    custom_filter = function(buf_number, buf_numbers)
+                        -- filter out filetypes you don't want to see
+                        if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+                            return true
+                        end
+                        -- filter out by buffer name
+                        if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+                            return true
+                        end
+                        -- filter out based on arbitrary rules
+                        -- e.g. filter out vim wiki buffer from tabline in your work repo
+                        if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+                            return true
+                        end
+                        -- filter out by it's index number in list (don't show first buffer)
+                        if buf_numbers[1] ~= buf_number then
+                            return true
+                        end
+                    end,
+                    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align="center"}}, -- | function , text_align = "left" | "center" | "right",
+                    show_buffer_icons = true, --| false, -- disable filetype icons for buffers
+                    show_buffer_close_icons = true, --| false,
+                    show_close_icon = true, --| false,
+                    show_tab_indicators = true, -- | false,
+                    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+                    -- can also be a table containing 2 custom separators
+                    -- [focused and unfocused]. eg: { '|', '|' }
+                    separator_style = "thin", --| "slant" | "thick" | "thin" | { 'any', 'any' },
+                    enforce_regular_tabs = false, --| true,
+                    always_show_bufferline = true, -- | false,
+                    sort_by =  'directory',  -- ,'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+                }
+            })
+	    end,
+
+	    keys = {
+            { "<leader>rr", ":BufferLineCycleNext<CR>", desc = "next buffer" },
+            { "<leader>ll", ":BufferLineCyclePrev<CR>", desc = "prev buffer" },
+	    }
     },
 
     { -- nvim-autopairs
