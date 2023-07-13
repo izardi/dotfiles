30b8440433e018f6403c8ec5841398870840a1ec
yu <yuhh.cs@gmail.com>
2023-07-13 22:59:44 +0800

    code_action

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -383,32 +383,32 @@ require("lazy").setup({
             -- Use LspAttach autocommand to only map the following keys
             -- after the language server attaches to the current buffer
             vim.api.nvim_create_autocmd('LspAttach', {
-              group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-              callback = function(ev)
-                -- Enable completion triggered by <c-x><c-o>
-                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                  callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-                -- Buffer local mappings.
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<leader>wl', function()
-                  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<leader>f', function()
-                  vim.lsp.buf.format { async = true }
-                end, opts)
-              end,
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wl', function()
+                      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<leader>fm', function()
+                      vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
             })
 
             lspconfig.clangd.setup({
@@ -417,12 +417,24 @@ require("lazy").setup({
                     "-j=4",
                     "--background-index",
                     "--clang-tidy",
+                    "--clang-tidy-checks=clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability-*,bugprone-*",
                     "--fallback-style=llvm",
                     "--all-scopes-completion",
                     "--completion-style=detailed",
                     "--header-insertion=iwyu",
                     "--header-insertion-decorators",
                     "--pch-storage=memory",
+                },
+                on_attach = function(client)
+                    -- 启用 clang-tidy
+                    client.resolved_capabilities.code_action = true
+                    client.resolved_capabilities.execute_command = true
+                end,
+                init_options = {
+                    clangdFileStatus = true,
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    semanticHighlighting = true
                 }
             })
         end
