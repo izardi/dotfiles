189a2ae1da5bab2a91a01603c67927c24cb0ecd5
zard <yuhh.cs@gmail.com>
2023-07-22 18:15:38 +0800

    remove unuse

--- config/fish/conf.d/other.fish
+++ config/fish/conf.d/other.fish
@@ -2,3 +2,11 @@ set -x CLUTTER_BACKEND wayland
 set -x XDG_CURRENT_DESKTOP sway
 set -x XDG_SESSION_DESKTOP sway
 set -x _JAVA_AWT_WM_NONREPARENTING 1
+
+function proxy
+  set -xg ALL_PROXY http://localhost:20172
+end
+
+function noproxy
+  set -e ALL_PROXY
+end
