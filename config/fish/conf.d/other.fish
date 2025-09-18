e2059046a8755e523e3dff451d2ab087d4b91337
zard <yuhh.cs@gmail.com>
2025-09-18 16:31:30 +0800

    update

--- config/fish/conf.d/other.fish
+++ config/fish/conf.d/other.fish
@@ -14,9 +14,7 @@ abbr -a rm rm -i
 abbr -a fet macchina
 abbr -a gg g++ -std=c++26 -Wall -pedantic -fsanitize=address,undefined -g
 abbr -a cl clang++ -std=c++26 -stdlib=libc++ -Wall -pedantic -fsanitize=address,undefined -g
-abbr -a mvi mpv --config-dir=/home/yu/.config/mpv-image-viewer
+abbr -a mvi mpv --config-dir=/home/yu/.config/mpv/mpv-image-viewer
 abbr -a za zathura
 abbr -a cr cargo run
 abbr -a pg pgcli -h db.zardi.eu.org -p 32026 -U postgres
-abbr -a vir virsh --connect qemu:///system
-abbr -a k kubectl
